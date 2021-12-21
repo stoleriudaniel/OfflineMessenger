88a56f31b058c678afe98c240c3c27b8490929da
stoleriudaniel <stoleriu.daniel47@gmail.com>
2021-12-21 15:56:34 +0200

    Create RepliedMessages table and rename some tables

--- server.c
+++ server.c
@@ -61,12 +61,17 @@ int main ()
 	sqlite3* db;
 	sqlite3_stmt* stmt;
 	sqlite3_open("DataBase.db", &db);
-	int rc = sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS Users(nume varchar(100), parola(100))", NULL, NULL, &err);
+	int rc = sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS Users(name varchar(100), password(100))", NULL, NULL, &err);
 	if(rc!=SQLITE_OK)
 	{
 		printf("eroare: %s", err);
 	}
-	rc=sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS NewMessages(numeDestinatar varchar(100), numeSursa varchar(100), idMesaj INT, mesaj varchar())", NULL, NULL, &err);
+	rc=sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS NewMessages(numeDestinatar varchar(100), nameFrom varchar(100), idMessage INT, message varchar())", NULL, NULL, &err);
+	if(rc!=SQLITE_OK)
+	{
+		printf("eroare: %s", err);
+	}
+	rc=sqlite3_exec(db,"CREATE TABLE IF NOT EXISTS RepliedMessages(name varchar(100), nameToReply varchar(100), idMessage INT, message varchar(100))", NULL ,NULL, &err);
 	if(rc!=SQLITE_OK)
 	{
 		printf("eroare: %s", err);
